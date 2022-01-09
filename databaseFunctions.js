ee8d8ad9ecca0dbce216a4fe68cbc695cd7ebfc1
ardayasar <erddoerdo12@gmail.com>
2022-01-09 04:54:55 +0300

    new

---
+++ databaseFunctions.js
@@ -0,0 +1,9 @@
+exports.checkUser = (username, password) => {
+    
+    return username.toString() + password.toString();
+}
+
+function checkSession(username, sessionID, privateKey){
+
+    return 0;
+}
--- server.js
+++ server.js
@@ -1,34 +1,173 @@
+// ~~~~IMPORTANT!~~~~ 
+// Server will never send any data with socket or publish any data
+// Client will be send `GET` request 
+// Server will lookup the time and capacity to correct
+// Then server will send OK -> DATA or ERROR -> Wait Time
+
+
 const express = require('express')
 const app = express()
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+var path = require('path');
+var genid=require('uuid');
 const port = 1604
+const database  = require('./databaseFunctions');
 
 app.use(express.json());
 app.use(logErrors)
 app.use(clientErrorHandler)
 app.use(errorHandler)
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-inData = {}
+var permissions = {
+  normal_user:{
+    renew_time: 15,
+    priority: 0,
+    max_capacity: 240
+  },
+  gold_user:{
+    renew_time: 10,
+    priority: 1,
+    max_capacity: 360
+  },
+  plat_user:{
+    renew_time: 5,
+    priority: 2,
+    max_capacity: 720
+  }
+}
 
-io.on('connection', (socket) => {
-    // socket.id = "abcdefgsnsdjlfnsdf"
-    // socket.handshake.auth.token
-    ID = socket.id;
-    console.log('client id - '+ socket.id); 
-  }); //<script src="/socket.io/socket.io.js"></script><script>  var socket = io();</script>
+// console.log(database.checkUser("aaaa", "bbbb"));
+
+var expireTime = new Date(Date.now() + (60 * 60 *24 * 1000)) 
+
+// ||||||||||||||||||||||
+// ||     Database     ||
+// ||||||||||||||||||||||
+
+// var connection = mysql.createConnection({
+// 	host     : 'localhost',
+// 	user     : 'root',
+// 	password : '',
+// 	database : 'thetanAPI'
+// });
+
+// ||||||||||||||||||||||
+// || Session & Cookie ||
+// ||||||||||||||||||||||
+
+// function createCookie() {
+//   res.cookie('sessionID', genid.v4(), {
+//     expires: new Date(Date.now() + 5 * 100000)
+//   });    
+// } 
+
+// || Main Page ||
 
 app.get('/', (req, res) => {
-  res.send('Hello World! <script src="/socket.io/socket.io.js"></script><script>  var socket = io();</script>')
+
+
+  res.send('Completed');
+
 })
 
-app.get('/submit-hero', (req, res) => {
-  res.status(200).send({'status': true});
+
+// || Login Get ||
+
+app.get('/ulgn_p',(req, res) => {
+  
+  var mail = req.query.mail;
+  var password = req.query.password;
+  
+  if(req.cookies.sessionID && !mail && !password){
+    if(req.cookies.expires === null){
+      res.clearCookie();
+      res.send({"success": false, "reason":{"code": 1, "msg": "Login expired!", 'show': true}}); 
+    }
+    else{
+
+      //Control database
+      if(true){
+
+        res.send({"success": true, 'show': true});
+      }
+      else{
+        res.send({"success": false, "reason":{"code": 1, "msg": "Login expired!", 'show': true}}); 
+      }
+      //Control database
+    }
+       
+  }
+  else{
+    if(mail && password){
+      res.clearCookie();
+      res.cookie('sessionID', genid.v4(), {
+        expires: expireTime
+      }); 
+      res.cookie('username', 'getFromDatabase', {
+        expires: expireTime
+      }); 
+      res.cookie('privateKey', genid.v4(), {
+        expires: expireTime
+      }); 
+
+      res.send({"success": false, "reason":{"code": 0, "msg": "Login Needed", 'show': true}});
+    }
+    else if(!req.cookies.sessionID && !mail && !password){
+      res.send({"success": false, "reason":{"code": 3, "msg": "Not logged in!", 'show': false}});
+    }
+    else if(mail && !password || !mail && password){
+      res.send({"success": false, "reason":{"code": 2, "msg": "Missing mail or password!"}, 'show': true});
+    }
+  }
+
+});
+
+app.get('/cc',(req, res) => {
+  res.clearCookie();
+  res.clearCookie();
+      res.cookie('sessionID', genid.v4(), {
+        expires: new Date(Date.now() + 10000) 
+      }); 
+      res.cookie('username', 'getFromDatabase', {
+        expires: expireTime
+      }); 
+      res.cookie('privateKey', genid.v4(), {
+        expires: expireTime
+      }); 
+  res.send("Created!")
+});
+
+
+// || Panel Page ||
+
+
+
+// |||||||||||||||||||||| SECRET
+// ||        API       || SECRET
+// |||||||||||||||||||||| SECRET
+
+app.get('/hero', (req, res) => {
+
+  // TODO: Control users permission
+
+  if(req.sess){
+    res.status(200).send(
+      {
+        'status': true
+      });
+  }
+
 })
 
-app.post('/submit-hero', (req, res) => {
+app.post('/hero', (req, res) => {
     try {
       if(req.body.Lkey == "permission_key"){
         res.send({'key': true, 'information': req.body})
@@ -43,6 +182,9 @@ app.post('/submit-hero', (req, res) => {
       res.status(401).send({'status': false})
     }
 });
+
+
+
 
 function logErrors (err, req, res, next) {
   console.error(err.stack)
@@ -61,8 +203,6 @@ function clientErrorHandler (err, req, res, next) {
     next(err)
   }
 }
-
-
 
 server.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
